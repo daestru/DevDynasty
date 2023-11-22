d9900856ce5959c0faea0826ec541aede2f508ce
Elijah F <etfreeman1s@semo.edu>
2023-11-21 21:51:49 -0600

    updateTemplates

--- CS445Portfolio/DB/updateTemplatesDB.php
+++ CS445Portfolio/DB/updateTemplatesDB.php
@@ -1,5 +1,38 @@
 <?php
-/*
-  Affect the templates table as indicated
-*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Database connection details
+    $host = "localhost";
+    $username = "cs445";
+    $password = "pass";
+    $database = "cs445portfolio";
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    $templateName = mysqli_real_escape_string($conn, $_POST["templateName"]);
+    $templateID = mysqli_real_escape_string($conn, $_POST["templateID"]);
+    $description = mysqli_real_escape_string($conn, $_POST["description"]);
+
+    if(!empty($templateID)){
+        $sql0 =  "INSERT INTO templates (templateName, templateID, description) VALUES('$templateName', '$templateID', '$description')";
+        if ($conn->query($sql0) === TRUE) {
+            echo "Email updated successfully!";
+        } else {
+            echo "Error updating email: " . $conn->error;
+        }
+    }
+
+    else{
+        $sql0 =  "DELETE FROM templates WHERE templateName='$templateName'";
+        if ($conn->query($sql0) === TRUE) {
+            echo "Email updated successfully!";
+        } else {
+            echo "Error updating email: " . $conn->error;
+        }
+    }
+    
+    header("location: ../updateTemplatesForm.php");
+
+    
+}
 ?>

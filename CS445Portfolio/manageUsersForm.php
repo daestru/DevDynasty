ed34132cc8bf88299818415b0e8a7cbde901e3d1
Elijah F <etfreeman1s@semo.edu>
2023-11-21 20:59:56 -0600

    commits cool

--- CS445Portfolio/manageUsersForm.php
+++ CS445Portfolio/manageUsersForm.php
@@ -1,6 +1,43 @@
-<?php
-/*
-  Obtain user given information
-  Lead to manageUsersDB
-*/
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="flex-container-background">
+        <div class="flex-container">
+            <div class="flex-item-0">
+                <h1 id="form_header">User Manage</h1>
+            </div>
+        </div>
+
+        <div class="flex-container">
+            <div class="flex-item-1">
+                <form action="./DB/loginDB.php" method="post">
+                    <div class="flex-item-login">
+                        <h2>Change Email</h2>
+                    </div>
+
+                    <div class="flex-item">
+                        <input type="text" name="user_uname" placeholder="Enter Customer Username" required>
+                    </div>
+
+                    <div class="flex-item">
+                        <input type="text" name="new_mail" placeholder="Enter New Email" required>
+                    </div>
+
+                    <div class="flex-item">
+                        <button type="submit">Login</button>
+                        
+                    </div>
+                </form>
+                <a href="manageUsersForm.php"><button>UpdateEmail</button></a>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
+

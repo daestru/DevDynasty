a300f0096fc7c6ce3c6863f4c2399cdc5e34ae30
daestru <daniel.struttmann@gmail.com>
2023-11-28 20:30:30 -0600

    Update tipsAndTricks.php
    Editing Tips & Tricks

--- CS445Portfolio/src/tipsAndTricks.php
+++ CS445Portfolio/src/tipsAndTricks.php
@@ -5,7 +5,16 @@
 </head>
 
 <body>
-    <p>Tips and Tricks Here</p>
+    <p>1. Craft a compelling summary that highlights your key skills and achievements.</p>
+    <p>2. Tailor your resume to the specific job by emphasizing relevant experiences.</p>
+    <p>3. Use action verbs to start each bullet point in your work experience section.</p>
+    <p>4. Quantify your achievements with specific numbers to demonstrate impact.</p>
+    <p>5. Include a skills section showcasing both technical and soft skills.</p>
+    <p>6. Format your resume for readability, using a clean and professional layout.</p>
+    <p>7. Proofread carefully to eliminate typos and ensure flawless language.</p>
+    <p>8. Include a section for professional development and certifications.</p>
+    <p>9. Use a professional email address and include up-to-date contact information.</p>
+    <p>10. Tailor your resume for each application, aligning it with the job requirements.</p>
 </body>
 </html>
 

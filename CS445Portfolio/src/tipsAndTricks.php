1487d446aa2b99ff5c427cf03a0473ec7718b27b
daestru <daniel.struttmann@gmail.com>
2023-11-28 20:52:46 -0600

    Editing Numbering

--- CS445Portfolio/src/tipsAndTricks.php
+++ CS445Portfolio/src/tipsAndTricks.php
@@ -6,14 +6,14 @@
 
 <body>
     <h1>Tips and Tricks</h1>
-    
+
     <p>1. Provide compelling facts that highlight your key skills and achievements.</p>
     <p>2. Provide specific, individual job-related details.</p>
     <p>3. Utilize metrics to your advantage. Objective results speak louder than subjective "filler" information.</p>
     <p>4. Individually tailor a resume for each application.</p>
     <p>5. Include a section for professional development and certifications.</p>
-    <p>7. Proofread carefully. Grammar/spelling mistakes are an instant red flag.</p>
-    <p>9. Include current, professional, and accurate contact information.</p>
+    <p>6. Proofread carefully. Grammar/spelling mistakes are an instant red flag.</p>
+    <p>7. Include current, professional, and accurate contact information.</p>
 
 </body>
 </html>

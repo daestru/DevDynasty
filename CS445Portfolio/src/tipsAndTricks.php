61e9028f12f670ffba18610a74562d8cb100efec
daestru <daniel.struttmann@gmail.com>
2023-11-28 20:45:22 -0600

    Minor grammatical enhancements

--- CS445Portfolio/src/tipsAndTricks.php
+++ CS445Portfolio/src/tipsAndTricks.php
@@ -8,7 +8,7 @@
     <p>1. Provide compelling facts that highlight your key skills and achievements.</p>
     <p>2. Provide specific, individual job-related details.</p>
     <p>3. Utilize metrics to your advantage. Objective results speak louder than subjective "filler" information.</p>
-    <p>4. Individually tailor a resume for each application</p>
+    <p>4. Individually tailor a resume for each application.</p>
     <p>5. Include a section for professional development and certifications.</p>
     <p>7. Proofread carefully. Grammar/spelling mistakes are an instant red flag.</p>
     <p>9. Include current, professional, and accurate contact information.</p>

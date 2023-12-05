af486748427891fbe3018c64d8ad4da3580ab74c
daestru <daniel.struttmann@gmail.com>
2023-12-04 20:04:56 -0600

    Inserting test html to see how it looks.

--- CS445Portfolio/src/Templates/1.php
+++ CS445Portfolio/src/Templates/1.php
@@ -19,13 +19,59 @@ echo "<p>Name: " . $portfolio->getName() . "</p>";
 echo "<p>Description: " . $portfolio->getDescription(). "</p>";
 echo "<p>Skills: " . $portfolio->getSkills() . "</p>";
 echo "<p>Experience: " . $portfolio->getExperience() . "</p>";
-
-
-
-
-
-
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resume</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #000; /* Black background */
+            color: #fff; /* White text */
+            margin: 20px;
+        }
+
+        .resume-section {
+            padding: 20px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
+        }
+
+        h2 {
+            color: #fff; /* White text for section headers */
+        }
+
+        p {
+            margin: 10px 0;
+        }
+    </style>
+</head>
+<body>
+
+<div class="resume-section">
+    <h2>Name</h2>
+    <p><?php echo $portfolio->getName(); ?></p>
+</div>
+
+<div class="resume-section">
+    <h2>Description</h2>
+    <p><?php echo $portfolio->getDescription(); ?></p>
+</div>
+
+<div class="resume-section">
+    <h2>Skills</h2>
+    <p><?php echo $portfolio->getSkills(); ?></p>
+</div>
+
+<div class="resume-section">
+    <h2>Experience</h2>
+    <p><?php echo $portfolio->getExperience(); ?></p>
+</div>
+
+</body>
+</html>
